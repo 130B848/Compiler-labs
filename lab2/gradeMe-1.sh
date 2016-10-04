d1a56c4aa668475aeb7072a670bf9e0f2ea952c4
130B848 <592ldj227@gmail.com>
2016-10-05 02:02:42 +0800

    lab2 finished

--- lab2/gradeMe-1.sh
+++ lab2/gradeMe-1.sh
@@ -3,16 +3,16 @@ BIN=lextest
 REFOUTDIR=refs-1
 TESTCASEDIR=./testcases
 
-rm -f _tmp.txt .tmp.txt	
+rm -f _tmp.txt .tmp.txt
 make clean >& /dev/null
 make > /dev/null
 if [[ $? != 0 ]]; then
-	echo "[-_-]$ite: Compile Error"		
+	echo "[-_-]$ite: Compile Error"
 	exit 123
-fi	
+fi
 
 for tcase in `ls $TESTCASEDIR`
-do		
+do
 	if [ ${tcase##*.} = "tig" ]; then
 		tfileName=${tcase##*/}
 
@@ -20,13 +20,12 @@ do
 		diff $DIFFOPTION _tmp.txt $REFOUTDIR/${tfileName%.*}.out >& .tmp.txt
 		if [ -s .tmp.txt ]; then
 			cat .tmp.txt
-			echo "[*_*]$ite: Output Mismatch [$tfileName]"				
-			rm -f _tmp.txt .tmp.txt	
+			echo "[*_*]$ite: Output Mismatch [$tfileName]"
+			rm -f _tmp.txt .tmp.txt
 			exit 234
 		fi
-	fi	
+	fi
 done
 
-rm -f _tmp.txt .tmp.txt	
+rm -f _tmp.txt .tmp.txt
 echo "[^_^]$ite: Pass"
-
